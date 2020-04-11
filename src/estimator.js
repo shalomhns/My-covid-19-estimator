f730fd5ea5b49a1d52c8f9926629f0a3f3c8699f
Samson Adeyemi <shalomhns@gmail.com>
2020-04-11 11:06:25 +0100

    start building estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,12 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) =>  {
+
+let {periodType, region, reportedCases, population, totalHospitalBeds} = data;
+
+return {
+  data,
+  impact: {currentlyinfected},
+  severeImpact: {}
+}
+};
 
 export default covid19ImpactEstimator;
