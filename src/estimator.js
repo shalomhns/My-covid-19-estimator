1c857efc084a188da1ebe03e52ba4f2370228b26
Samson Adeyemi <shalomhns@gmail.com>
2020-04-11 17:55:28 +0100

    start building estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,20 @@
-const covid19ImpactEstimator = (data) =>  {
-
-let {periodType, region, reportedCases, population, totalHospitalBeds} = data;
-
-return {
-  data,
-  impact: {currentlyinfected},
-  severeImpact: {}
-}
+const input = {
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
 };
-
+const covid19ImpactEstimator = (data) => {
+  // Challange 1:
+  const currentlyInfected = data.reportedCases * 10;
+  const severeImpact = data.reportedCases * 50;
+  const infectionsByRequestedTimeForImpact = currentlyInfected * 512;
+  const infectionsByRequestedTimeForSevereImpact = severeImpact * 512;
+  return {
+    data: input,
+    impact: {},
+    severeImpact: {}
+  };
+};
 export default covid19ImpactEstimator;
